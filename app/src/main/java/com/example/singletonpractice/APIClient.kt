1344efb92cea588a624e5da0800dc7278bd3ecc6
albtoul <albtoul-kaseem@hotmail.com>
2021-10-18 15:57:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/singletonpractice/APIClient.kt
@@ -0,0 +1,17 @@
+package com.example.singletonpractice
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object APIClient {
+    var retrofit: Retrofit? = null
+
+    fun getClient(): Retrofit? {
+        retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASEURL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit
+    }
+}
